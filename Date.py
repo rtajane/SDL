314854024c1863b3eb763ce3bb436d49ebc8dbda
rtajane <40752449+rtajane@users.noreply.github.com>
2018-07-13 10:47:08 +0530

    Add files via upload

---
+++ Date.py
@@ -0,0 +1,7 @@
+import datetime
+t=datetime.time(1,2,3);
+print("Hours :",t.hour);
+print("Minutes :",t.minute);
+print("Seconds :",t.second);
+#print("Miliseconds :",t.millisecond);
+print("tzinfo :",t.tzinfo);
